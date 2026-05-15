1dbe879725b5d19d193882b1a2f67e8082d9f29d
Ramon Asuncion <asuncionbatista@gmail.com>
2026-05-15 01:12:35 -0400

    Use uv pip in setup scripts

--- src/setup.py
+++ src/setup.py
@@ -4,12 +4,24 @@ from venv import create
 from subprocess import run
 
 venv_dir = "./stt-venv"
-pip_path = os.path.join(venv_dir, "bin", "pip")
+uv_path = shutil.which("uv")
+assert uv_path, "uv not found in PATH"
 
 # TODO(d809): re-sync packages if requirements.txt changes after venv is created
 if not os.path.isdir(venv_dir):
     create(venv_dir, with_pip=True)
-    run([pip_path, "install", "-r", os.path.abspath("requirements.txt")], check=True)
+    run(
+        [
+            uv_path,
+            "pip",
+            "install",
+            "--python",
+            venv_dir,
+            "-r",
+            os.path.abspath("requirements.txt"),
+        ],
+        check=True,
+    )
 
 os.makedirs("private", exist_ok=True)
 
